7ac87e92fb4e0b8ac67438575bb9af2fab205915
Александр Дроботов <avdrobotov@gmail.com>
2026-06-04 16:34:27 +0300

    Add files via upload

---
+++ 01 Task/BigAddApp.dpr
@@ -0,0 +1,62 @@
+program BigAddApp;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  uBigMath in 'uBigMath.pas';
+
+var
+  F : TextFile;
+  N1, N2 : TBigInt;
+begin
+  try
+    //  создаем объекты - большие числа
+    N1 := TBigInt.Create;
+    N2 := TBigInt.Create;
+    try
+
+      //  открываем файл на чтение
+      Assign(f, 'numbers.txt');
+      Reset(f);
+
+      try
+
+        //  Читаем числа
+        N1.ReadFromFile(f);
+        N2.ReadFromFile(f);
+
+      finally
+        //  Закрываем файл на чтение
+        CloseFile(f);
+      end;
+
+      //  Складываем два числа
+      N1.Add(N2);
+
+      //  открываем файл на запись
+      Append(f);
+      try
+
+        //  Добавляем итог в файл
+        Writeln(f);
+        N1.WriteToFile(f);
+
+        Flush(f);
+
+      finally
+        //  закрываем файл на запись
+        CloseFile(f);
+      end;
+
+    finally
+      //  Освобождаем память
+      N1.Free;
+      N2.Free;
+    end;
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
