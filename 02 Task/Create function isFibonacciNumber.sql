46e0962d03ee3f27dbbd4c0840bdc0d3c6e8ebe2
avdrobotov <avdrobotov@atomicmail.io>
2026-06-04 22:01:20 +0300

    Task 2

---
+++ 02 Task/Create function isFibonacciNumber.sql
@@ -0,0 +1,48 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		А.В. Дроботов
+-- Create date: 04.06.2026
+-- Description:	Проверка значениям на число Фибаначчи
+-- =============================================
+CREATE FUNCTION isFibonacciNumber
+(
+	-- Параметры функции
+	@pnumValueForCheck BIGINT
+)
+RETURNS BIT
+AS
+BEGIN
+	-- временное хранилище возвращаемого значения
+	DECLARE @boolResult BIT
+	-- переменные для расчета числ Фибаначчи
+	DECLARE @numN1 BIGINT = 1
+	DECLARE @numN2 BIGINT = 1
+	DECLARE @numN3 BIGINT = 1
+
+	-- по умолчанию определим что число не принадлежит множеству чисел Фибаначчи
+	SELECT @boolResult = 0
+
+	IF @pnumValueForCheck >= 1
+	BEGIN
+	  WHILE @pnumValueForCheck > @numN2
+	  BEGIN
+	    SET @numN3 = @numN1 + @numN2
+		SET @numN1 = @numN2
+		SET @numN2 = @numN3
+	  END
+	  IF @pnumValueForCheck = @numN2
+	  BEGIN
+	    SET @boolResult = 1
+	  END
+	END
+
+	-- Return the result of the function
+	RETURN @boolResult
+
+END
+GO
+
